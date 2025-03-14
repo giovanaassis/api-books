a9114abac3846df2581c64dd6b797c8da4582bc8
giovanaassis <giovana_2011_11@hotmail.com>
2025-03-14 00:22:45 -0300

    feat: add multiple validations yup

--- src/server/controllers/livros/Create.ts
+++ src/server/controllers/livros/Create.ts
@@ -3,24 +3,30 @@ import * as yup from "yup";
 
 interface ILivro {
   titulo: string;
+  genero: string;
 }
 
 const bodyValidation: yup.Schema<ILivro> = yup.object().shape({
   titulo: yup.string().required().min(2),
+  genero: yup.string().required(),
 });
 
 export const create = async (req: Request<{}, {}, ILivro>, res: Response) => {
   let validatedData: ILivro | undefined = undefined;
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
   } catch (err) {
     const error = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    res.json({
-      errors: {
-        default: error.message,
-      },
+    error.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
     });
+
+    res.json({ errors });
   }
 
   console.log(validatedData);
