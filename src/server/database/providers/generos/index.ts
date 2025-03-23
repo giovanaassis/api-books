6e269b2659275bc7a41fbe86422748310d2f3a4e
giovanaassis <giovana_2011_11@hotmail.com>
2025-03-23 04:12:20 -0300

    feat: add count provider for genero

--- src/server/database/providers/generos/index.ts
+++ src/server/database/providers/generos/index.ts
@@ -1,7 +1,9 @@
 import * as getAll from "./GetAll";
 import * as getById from "./GetById";
+import * as count from "./Count";
 
 export const GenerosProvider = {
   ...getAll,
   ...getById,
+  ...count,
 };
