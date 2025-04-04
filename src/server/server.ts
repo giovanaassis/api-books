9acc206335845ce25eb15110bbfa2b93b2624176
giovanaassis <giovana_2011_11@hotmail.com>
2025-04-04 19:55:17 -0300

    feat: config cors

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import "./shared/services/TranslationsYup";
 import router from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+  }),
+);
+
 app.use(express.json());
 
 app.use(router);
